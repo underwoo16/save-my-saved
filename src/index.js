b30ef12af83ab6283f42b19327d654e704ef9f6d
Chandler Underwood <underwoo16@up.edu>
2020-04-18 15:38:43 -0700

    parse a minimum amount of info from each saved item based on its type

--- src/index.js
+++ src/index.js
@@ -12,9 +12,17 @@ const REDDIT_ACCESS_URL = 'https://www.reddit.com/api/v1/access_token';
 const REDDIT_AUTH_URL = 'https://oauth.reddit.com';
 const REDDIT_SAVED_URL = `${REDDIT_AUTH_URL}/user/${username}/saved`;
 
-// TODO: parsing values
-const t3keysToSave = ['title', 'permalink', 'url'];
-const t1keysToSave = ['link_title', 'permalink'];
+// parsing values
+const KEYS_TO_SAVE = {
+  't3': ['title', 'permalink', 'url'],
+  't1': ['link_title', 'permalink']
+};
+
+async function getJsonResponse(url, options) {
+  const response = await fetch(url, options);
+  const json = await response.json();
+  return json;
+}
 
 async function getAccessToken() {
   const authData = `${client_id}:${secret_id}`;
@@ -24,12 +32,11 @@ async function getAccessToken() {
   const headers = {
     Authorization: `Basic ${base64data}`,
     "Content-Type": "application/x-www-form-urlencoded"
-  }
-
+  };
   const body = `grant_type=password&username=${username}&password=${password}`;
-  const response = await fetch(REDDIT_ACCESS_URL, { method: 'POST', headers: headers, body: body});
-  const json = await response.json();
-  return json.access_token;
+
+  const response = await getJsonResponse(REDDIT_ACCESS_URL, { method: 'POST', headers: headers, body: body});
+  return response.access_token;
 }
 
 async function getAuthorizationSettings() {
@@ -41,27 +48,40 @@ async function getAuthorizationSettings() {
 }
 
 async function getSavedListings() {
-  const requestSettings = await getAuthorizationSettings();
-  const response = await fetch(REDDIT_SAVED_URL, requestSettings);
-  const json = await response.json();
+  const requestOptions = await getAuthorizationSettings();
+  const json = await getJsonResponse(REDDIT_SAVED_URL, requestOptions);
 
   let listings = json.data.children;
   let after = json.data.after;
 
-  // todo: make a getNextListings recursive function? base case after===null?
+  // todo/enhancement: make a getNextListings recursive function? base case after===null?
   while (after) {
-    const afterUrl = `${REDDIT_SAVED_URL}?after=${after}`;
-    const afterResponse = await fetch(afterUrl, requestSettings);
-    const afterJson = await afterResponse.json();
+    const afterJson = await getJsonResponse(`${REDDIT_SAVED_URL}?after=${after}`, requestOptions);
     listings.push(...afterJson.data.children);
     after = afterJson.data.after;
   }
   return listings;
 }
 
+// todo/enhancement: use map here?
+function parseSavedListings(listings) {
+  return listings.map(parseSavedItem);
+};
+
+// todo/enhancement: there is probably a fancier way to map a subset of keys from one object to another
+function parseSavedItem(item) {
+  let newItem = {};
+  for (const key of KEYS_TO_SAVE[item.kind]) {
+    newItem[key] = item.data[key];
+  }
+  return newItem;
+}
+
+// TODO: write a driver that requests listings, parses them, then writes them to csv
 async function printStuff() {
   const stuff = await getSavedListings();
-  console.log(stuff);
+  const saved = parseSavedListings(stuff);
+  console.log(saved);
 }
 
 printStuff();
