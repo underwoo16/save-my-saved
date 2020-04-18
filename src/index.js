d577579a65d593bbeadce2496ae494d6eb2d559d
Chandler Underwood <underwoo16@up.edu>
2020-04-18 15:02:56 -0700

    fetch all saved listings

--- src/index.js
+++ src/index.js
@@ -1,44 +1,67 @@
 const fetch = require('node-fetch');
 
-// api access values DO NOT share these
+// API auth values DO NOT share these
 const client_id = '';
 const secret_id = '';
 const username = '';
 const password = '';
 
+
 // API access values
 const REDDIT_ACCESS_URL = 'https://www.reddit.com/api/v1/access_token';
 const REDDIT_AUTH_URL = 'https://oauth.reddit.com';
-const SAVED_PATH = `/user/${username}/saved`;
+const REDDIT_SAVED_URL = `${REDDIT_AUTH_URL}/user/${username}/saved`;
 
-// base64 encode AUTHORIZATION header
-let authData = `${client_id}:${secret_id}`;
-let buff = Buffer.from(authData);
-let base64data = buff.toString('base64');
+// TODO: parsing values
+const t3keysToSave = ['title', 'permalink', 'url'];
+const t1keysToSave = ['link_title', 'permalink'];
 
-const headers = {
-  Authorization: `Basic ${base64data}`,
-  "Content-Type": "application/x-www-form-urlencoded"
+async function getAccessToken() {
+  const authData = `${client_id}:${secret_id}`;
+  const buff = Buffer.from(authData);
+  const base64data = buff.toString('base64');
+
+  const headers = {
+    Authorization: `Basic ${base64data}`,
+    "Content-Type": "application/x-www-form-urlencoded"
+  }
+
+  const body = `grant_type=password&username=${username}&password=${password}`;
+  const response = await fetch(REDDIT_ACCESS_URL, { method: 'POST', headers: headers, body: body});
+  const json = await response.json();
+  return json.access_token;
 }
 
-const body = `grant_type=password&username=${username}&password=${password}`;
-
-fetch(REDDIT_ACCESS_URL, { method: 'POST', headers: headers, body: body})
-  .then((res) => {
-     return res.json();
-})
-.then((json) => {
-  console.log(json);
+async function getAuthorizationSettings() {
+  const accessToken = await getAccessToken();
   const headers = {
-    Authorization: `bearer ${json.access_token}`
+    Authorization: `bearer ${accessToken}`
   };
-  const url = `${REDDIT_AUTH_URL}${SAVED_PATH}`;
-  console.log(url);
-  fetch(url, { method: 'GET', headers: headers })
-    .then((res) => {
-      return res.json();
-    })
-    .then((json) => {
-      console.log(json);
-    });
-});
+  return { method: 'GET', headers: headers };
+}
+
+async function getSavedListings() {
+  const requestSettings = await getAuthorizationSettings();
+  const response = await fetch(REDDIT_SAVED_URL, requestSettings);
+  const json = await response.json();
+
+  let listings = json.data.children;
+  let after = json.data.after;
+
+  // todo: make a getNextListings recursive function? base case after===null?
+  while (after) {
+    const afterUrl = `${REDDIT_SAVED_URL}?after=${after}`;
+    const afterResponse = await fetch(afterUrl, requestSettings);
+    const afterJson = await afterResponse.json();
+    listings.push(...afterJson.data.children);
+    after = afterJson.data.after;
+  }
+  return listings;
+}
+
+async function printStuff() {
+  const stuff = await getSavedListings();
+  console.log(stuff);
+}
+
+printStuff();
